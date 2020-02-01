fc345493bc060822c5178d0b4ab648f0903e9b29
Dhvanesh Shah <dhvaneshshah97@gmail.com>
2020-02-01 10:42:50 -0800

    Hello World

---
+++ public/App.js
@@ -0,0 +1,32 @@
+// import React, { Component } from 'react';
+var contentNode = document.getElementById('contents');
+class App extends React.Component {
+    render() {
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "h1",
+                null,
+                "Dhvanesh Hetalkumar Shah"
+            ),
+            React.createElement("img", { src: "./myself.jpg" }),
+            React.createElement(
+                "p",
+                null,
+                "Hello, I am Dhvanesh Shah. Currently, I am pursuing my Master degree in Computer Science from San Diego State University. I love to listen songs and playing games in my leisure time.  "
+            ),
+            React.createElement(
+                "button",
+                null,
+                React.createElement(
+                    "a",
+                    { href: "https://github.com/dhvanesh97?tab=repositories" },
+                    "VIEW MY GITHUB REPO"
+                )
+            )
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(App, null), contentNode);
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import React, { Component } from 'react';
+var contentNode = document.getElementById('contents');
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Dhvanesh Hetalkumar Shah</h1>
+                <img src="./myself.jpg"></img>
+                <p>Hello, I am Dhvanesh Shah. Currently, I am pursuing my Master degree in Computer Science from San Diego State University. I love to listen songs and playing games in my leisure time.  </p>
+                <button><a href="https://github.com/dhvanesh97?tab=repositories">VIEW MY GITHUB REPO</a></button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, contentNode);
