00c229f3bb5af939671054862c0d4e67a74ab7a6
Dhvanesh Shah <dhvaneshshah97@gmail.com>
2020-02-02 21:17:22 -0800

    updated css,jsx

--- public/App.js
+++ public/App.js
@@ -4,17 +4,17 @@ class App extends React.Component {
     render() {
         return React.createElement(
             "div",
-            null,
+            { className: "myinfo" },
             React.createElement(
-                "h1",
+                "h2",
                 null,
-                "Dhvanesh Hetalkumar Shah"
+                "Dhvanesh Shah"
             ),
-            React.createElement("img", { src: "./myself.jpg" }),
+            React.createElement("img", { src: "./myself.jpg", alt: "Head Shot" }),
             React.createElement(
                 "p",
                 null,
-                "Hello, I am Dhvanesh Shah. Currently, I am pursuing my Master degree in Computer Science from San Diego State University. I like to learn new things about technologies and implement them. Besides what I do, I love to listen songs and playing games in my leisure time.  "
+                "Hello, I am Dhvanesh Shah. Currently, I am pursuing my Master degree in Computer Science from San Diego State University. I like to learn new things about technologies and implement them. Besides my academics, I love to listen songs and playing games in my leisure time.  "
             ),
             React.createElement(
                 "button",
--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ var contentNode = document.getElementById('contents');
 class App extends React.Component{
     render(){
         return(
-            <div>
-                <h1>Dhvanesh Hetalkumar Shah</h1>
-                <img src="./myself.jpg"></img>
-                <p>Hello, I am Dhvanesh Shah. Currently, I am pursuing my Master degree in Computer Science from San Diego State University. I like to learn new things about technologies and implement them. Besides what I do, I love to listen songs and playing games in my leisure time.  </p>
+            <div className="myinfo">
+                <h2>Dhvanesh Shah</h2>
+                <img src="./myself.jpg"  alt="Head Shot"></img>
+                <p>Hello, I am Dhvanesh Shah. Currently, I am pursuing my Master degree in Computer Science from San Diego State University. I like to learn new things about technologies and implement them. Besides my academics, I love to listen songs and playing games in my leisure time.  </p>
                 <button><a href="https://github.com/dhvanesh97?tab=repositories">VIEW MY GITHUB REPO</a></button>
             </div>
         );
